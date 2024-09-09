0c13c7e02dbc4cb3b6d9a2ae2fd1edf0d581c8b9
cbrassard-ulaval <99917301+CharlieBrassard@users.noreply.github.com>
2024-09-09 19:37:01 -0400

    etape1

--- src/domain/Clinic.java
+++ src/domain/Clinic.java
@@ -1,15 +1,22 @@
 package src.domain;
+import java.util.LinkedList;
+import java.util.List;
 
 import src.enums.VisibleSymptom;
 
 public class Clinic {
-    public Clinic(/* ... */) {
+    private LinkedList<String> doctorPatients;
+    private LinkedList<String> radiologyPatients;
+    public Clinic() {
+        this.doctorPatients = new LinkedList<String>();
+        this.radiologyPatients = new LinkedList<String>();
     }
 
     public void triagePatient(String name, int gravity, VisibleSymptom visibleSymptom) {
-        // TODO
+        doctorPatients.add(name);
+
+        if (visibleSymptom == VisibleSymptom.BROKEN_BONE || visibleSymptom == VisibleSymptom.SPRAIN ){
+            radiologyPatients.add(name);
+        }
     }
-
-    // D'autres méthodes peuvent être nécessaires
-
 }
--- src/main.java
+++ src/main.java
@@ -1,12 +1,21 @@
 package src;
 
+import src.domain.Clinic;
 import src.enums.TriageType;
+import src.enums.VisibleSymptom;
+
+import java.util.LinkedList;
 
 public class main {
     public static void main(String[] args) {
         // Ceci n'est pas un test!! C'est un exemple d'utilisation.
         TriageType doctorTriageType = TriageType.FIFO;
         TriageType radiologyTriageType = TriageType.FIFO;
+
+        Clinic clinic = new Clinic();
+        clinic.triagePatient("bob", 2, VisibleSymptom.CHEST_PAIN);
+
+
 
         // Clinic clinic = new Clinic(doctorTriageType, radiologyTriageType /* , ... */)
         // clinic.triagePatient("John", 4, VisibleSymptom.MIGRAINE);
